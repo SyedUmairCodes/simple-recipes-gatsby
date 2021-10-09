17f8ab3df2fbfe7f5499af85b2768f8786a182bb
Syed Umair Ali <syedumairali.617@gmail.com>
2021-10-09 20:24:31 +0500

    Dummy site-metadata added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simple Recipes",
+    description: "Simple and quick cooking recipes resource",
+    author: "@SyedUmairAli",
+    person: { name: "Syed Umair Ali", age: 21 },
+    simpleData: ["item1", "item2"],
+    complexData: [
+      { name: "Syed Umair Ali", age: 21 },
+      { name: "Syed Uzair Ali", age: 12 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
