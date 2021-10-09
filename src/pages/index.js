a8823f076e5720969b512d9b7ed0ac656f80733c
Syed Umair Ali <syedumairali.617@gmail.com>
2021-10-09 19:14:36 +0500

    Hero section completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Index = () => {
   return (
-    <>
-      <Layout>
-        <h1>Home page</h1>
-      </Layout>
-    </>
+    <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="bakery"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Simple Recipes</h1>
+              <h4>Simple and Quick recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
+    </Layout>
   )
 }
 
